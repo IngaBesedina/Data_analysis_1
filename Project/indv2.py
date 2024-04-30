367aca62bd65ed3fd10767b4f36aa3063c1b9c90
IngaBesedina <inga.besedina@mail.ru>
2024-04-30 16:13:36 +0300

    add py files

--- Project/indv2.py
+++ Project/indv2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import sys
 
 
@@ -9,14 +8,17 @@ def tail(filename):
     try:
         with open(filename, "r", encoding="utf-8") as file:
             sentences = file.readlines()
-            if len(sentences) > 10:
-                for sentence in sentences[-10:]:
-                    print(sentence, end="")
-            else:
-                for sentence in sentences:
-                    print(sentence, end="")
+
+        if len(sentences) > 10:
+            for sentence in sentences[-10:]:
+                print(sentence, end="")
+        else:
+            for sentence in sentences:
+                print(sentence, end="")
+
     except FileNotFoundError:
         print(f"File {filename} not found")
+
 
 def main():
     if len(sys.argv) != 2:
@@ -24,7 +26,6 @@ def main():
     else:
         tail(sys.argv[1])
 
+
 if __name__ == "__main__":
     main()
-
-
